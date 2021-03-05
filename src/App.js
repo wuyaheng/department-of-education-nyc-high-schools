550e246775232b7aa7c0f5d87b49609bb0c451b6
wuyaheng <wuyaheng2016@gmail.com>
2021-03-05 11:22:58 -0500

    add a selector

--- src/App.js
+++ src/App.js
@@ -1,28 +1,88 @@
 import React, { Component } from 'react';
 import './App.css';
-import Map from "./components/Map/index"
-import geodata from "./data/nyc.geojson"
-import axios from "axios"
+import Map from "./components/Map/index";
+import ChooseNTA from "./components/ChooseNTA/index";
+import geodata from "./data/nyc.geojson";
+import axios from "axios";
+
+const ALLNEIGHBORHOOD = "All Neighborhood"
 
 class App extends Component {
   state = {
-    nta: []
+    geo: [],
+    nta: [],
+    sel_nta: "",
+    schools: [],
+    filteredSchools: [] 
   }
 
   componentDidMount() {
-      this.fetchdata()  
+    this.setState(
+      {
+        sel_nta: ALLNEIGHBORHOOD,
+      },
+      () => {
+      this.fetchSchools()
+      });
+    this.fetchnta();
+    this.fetchdata()
   }
 
   fetchdata = async () => {
     try {
       const res = await axios.get(geodata);
       this.setState({
-        nta: res.data.features
+        geo: res.data.features
       });
     } catch (error) {
       console.log(error)
     }
   } 
+
+
+  fetchnta = async () => {
+    try {
+      const res = await axios.get(
+        'https://data.cityofnewyork.us/resource/qpj9-6qjn.json?$group=nta&$select=nta'
+      );
+      const dropdownNta = res.data.map((x) => x.nta)
+      const dropdown = [ALLNEIGHBORHOOD,...dropdownNta]
+      this.setState({
+        nta: dropdown
+      });
+    } catch (error) {
+      console.log(error)
+    }
+  } 
+
+  handleInputChange = (event) => {
+    this.setState(
+      {
+        sel_nta: event.target.value
+      },
+      () => {
+      this.fetchSchools() 
+      })
+
+  }
+
+
+  fetchSchools = async () => { 
+    let options = {}
+    if (this.state.sel_nta !== ALLNEIGHBORHOOD) {
+      options = { 
+        params: {
+          nta: this.state.sel_nta 
+        }
+      }
+    }
+    const res = await axios.get('https://data.cityofnewyork.us/resource/qpj9-6qjn.json',options)
+
+    this.setState({
+      schools: res.data
+    })
+  }
+
 
   render() { 
       return (
@@ -33,13 +93,18 @@ class App extends Component {
             </div>
           </nav>
         
-        <div className="container-fluid">
+        <div className="container-fluid mt-2">
         <div className="row">
         <div className="col-md-2">
-
+        <div className="card mt-0" style={{height: "85vh"}}>
+      <div className="searchCard">
+      <p className="pt-1">&nbsp; <b>Choose a Neighborhood</b></p> 
+        <ChooseNTA results={this.state.nta} handleInputChange={this.handleInputChange} /> 
+      </div>
+        </div>
         </div>
         <div className="col-md-5">
-          <Map results = {this.state.nta}/> 
+          <Map results = {this.state.geo}/> 
         </div>
         <div className="col-md-5">
     
